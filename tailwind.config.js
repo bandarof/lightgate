22e57ab9cf6268e9eac114835668ce1fff80cdfc
Bandarof <bandarof@gmail.com>
2026-01-25 18:32:42 +0100

    fix: expand tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
