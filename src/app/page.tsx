672e61b714cbb66386f2d6852e1d3ff32756ae63
Bandarof <bandarof@gmail.com>
2026-01-25 18:46:56 +0100

    section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,3 @@
-export default function Home(){return(<main><section className='section'><span className='section-number'>01</span><h1 className='section-title'>Illuminating the World Through Culture</h1><p className='editorial'>Lightgate is a global cultural platform.</p></section></main>)}
+export default function Home(){return(<main><section className="section hero">
+  <div className="container">
+<span className='section-number'>01</span><h1 className='section-title'>Illuminating the World Through Culture</h1><p className='editorial'>Lightgate is a global cultural platform.</p></div></section></main>)}
