06dc702f2e28853250cb1e5f8388f9d96d25f309
Bandarof <bandarof@gmail.com>
2026-01-25 18:51:51 +0100

    Flagship

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,27 @@
+import "./globals.css";
+import Link from "next/link";
 
-import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+
+        <header className="nav">
+          <div className="container nav-inner">
+            <div className="logo">LIGHTGATE</div>
+            <nav>
+              <Link href="/">Home</Link>
+              <Link href="/about">About</Link>
+              <Link href="/services">Services</Link>
+              <Link href="/portfolio">Portfolio</Link>
+              <Link href="/contact">Contact</Link>
+            </nav>
+          </div>
+        </header>
+
+        {children}
+
+      </body>
     </html>
   );
 }
