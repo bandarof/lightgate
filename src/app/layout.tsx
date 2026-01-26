5159ec94ce9989e693a93182762ed889d64d6dd1
Bandarof <bandarof@gmail.com>
2026-01-26 17:51:50 +0100

    toggle fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
 import "./globals.css";
-import Link from "next/link";
+import Header from "@/components/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
 
-        <header className="nav">
-          <div className="container nav-inner">
-            <div className="logo">LIGHTGATE</div>
-            <nav>
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Link href="/services">Services</Link>
-              <Link href="/portfolio">Portfolio</Link>
-              <Link href="/contact">Contact</Link>
-            </nav>
-          </div>
-        </header>
+        <Header />
 
         {children}
 
